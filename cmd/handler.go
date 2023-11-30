8db1332d6468c9a43d127841694b813a60fc39c0
Kerimberdi Saparow <keshasaparow@mail.ru>
2023-11-30 20:58:19 +0500

    command_files

---
+++ cmd/handler.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func hello(){
+	fmt.Println("Hello Kerim")
+}
+
+var handler = &cobra.Command{
+	
+	Use: 	"hanlder",
+	Short:  "Makes handler",
+	Long:	"Creates handler with custom name",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello world")
+	},
+}
+
--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 import (
    "fmt"
-   "os"    //import fmt and os package
+      //import fmt and os package
 )
 
-func main() {
-   directory, err := os.Getwd()    //get the current directory using the built-in function
-   if err != nil {
-      fmt.Println(err) //print the error if obtained
-   }
-   fmt.Println("Current working directory:", directory) //print the required directory
+// func main() {
+//    directory, err := os.Getwd()    //get the current directory using the built-in function
+//    if err != nil {
+//       fmt.Println(err) //print the error if obtained
+//    }
+//    fmt.Println("Current working directory:", directory) //print the required directory
+// }
+
+
+func main(){
+	
 }
